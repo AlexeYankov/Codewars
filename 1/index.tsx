ae3baa868b7ce0dc75072d8539a30a78c560a267
Alexey Yankov <Yankovav.wm@gmail.com>
2022-05-25 11:35:59 +0300

    added few tasks

--- 1/index.tsx
+++ 1/index.tsx
@@ -1,17 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './p1-main/m1-ui/u1-app/App'
-import * as serviceWorker from './serviceWorker'
+// 24.05.22
+1.
+Instructions
+Code as fast as you can! You need to double the integer and return it.
+    function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i;
+}
+    Solution
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+}
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById("root")
-)
+2.
+Instructions
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+    Examples
+    makeNegative(1);    // return -1
+    makeNegative(-5);   // return -5
+    makeNegative(0);    // return 0
+    makeNegative(0.12); // return -0.12
+
+function makeNegative(num) {
+    // Code?
+}
+
+    Solution
+function makeNegative(num) {
+    return num < 0 ? num : -num;
+    // Code?
+}
+
+3.
+Instructions
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+    function getAge(inputString){
+// return the girl's correct age as an integer. Happy coding :)
+}
+
+Solution
+function getAge(inputString){
+// return the girl's correct age as an integer. Happy coding :)
+    const inputValue = inputString.slice(0,1)
+    const yourAge = (inputValue) => {
+        if (inputValue<10) {
+            inputValue
+        }
+        else ('You are too old')
+    }
+    const someText = () => ' years old'
+    const sumOfText = yourAge + someText
+    console.log(sumOfText)
+}
+
