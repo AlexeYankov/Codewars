281fcc3dd8804e5bdf99f867feba2b6d09816c4a
YankovAV <YankovAV.wm@gmail.com>
2023-01-18 16:42:37 +0300

    sfwfe

--- 1/index.tsx
+++ 1/index.tsx
@@ -1227,5 +1227,27 @@
 // }
 // solution("abracadabra")
 
+// 18.01.23
+// 53. Find The Parity Outlier
 
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// function findOutlier(integers){
+//     //your code here
+//     return integers.reduce(function(acc, el) {
+//         if (integers[0]%2 == 0) {
+//             el%2 == 0 ? "" : acc.push(el)
+//         };
+//         if (integers[0]%2 !== 0) {
+//             el%2 !== 0 ? acc.push(el) : ""
+//         };
+//         return acc;
+//     }, [])[0]
+//   }
