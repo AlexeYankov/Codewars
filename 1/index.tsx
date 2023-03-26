68dad017977d3b87b0ec100077231e0ff1db02fd
YankovAV <YankovAV.wm@gmail.com>
2023-03-26 10:41:05 +0300

    fefe

--- 1/index.tsx
+++ 1/index.tsx
@@ -1444,3 +1444,49 @@
 //   return "";
 // }
 // order("is2 Thi1s T4est 3a");
+
+// 04.03.23
+
+// 60. Count the number of Duplicates
+
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+
+// function duplicateCount(text) {
+//   const textTemp = text.toLowerCase().split("");
+//   if (!textTemp.length) {
+//     return 0;
+//   } else {
+//     let tempArray = text;
+//     let arrayOfUnique = tempArray
+//       .toLowerCase()
+//       .split("")
+//       .reduce(function (acc, el) {
+//         acc.includes(el) ? "" : acc.push(el);
+//         return acc;
+//       }, []);
+//     let arrayOfData1 = arrayOfUnique.reduce(function (acc, el) {
+//       let count = text.toLowerCase().split("").reduce(function (acc1, el1) {
+//         el == el1 ? acc1++ : "";
+//         return acc1;
+//       }, 0);
+//     if (count>1) {
+//     acc.push([el, count]);
+      
+//     }
+//     else {""}
+//       return acc;
+//     }, []);
+//     return arrayOfData1.length;
+//   }
+// }
